aee55b8b72c987ce204490a387231ec6a1363377
Kyle W. Givler <xvegan88x@gmail.com>
2020-05-04 20:46:00 -0400

    Another Example

--- Splatting.ps1
+++ Splatting.ps1
@@ -1,6 +1,28 @@
-# Define paramaters of a command before calling the command
+# Define paramaters of a command before calling the command - Hastable prefixed with @symbol
 $getProcess = @{
     Name = 'explorer'
 }
 
 Get-Process @getProcess
+
+
+# Longer example to add a Scheduled Task at Midnight
+$newTaskAction = @{
+    Execute = 'pwsh.exe'
+    Argument = 'Write-Host "Hello World"'
+}
+
+$newDailyTrigger =@{
+    Daily = $true;
+    At = '00:00:00'
+}
+
+$registerTask = @{
+    TaskName    = 'TaskName'
+    Action      = New-ScheduledTaskAction @newTaskAction
+    Trigger     = New-ScheduledTaskTrigger @newDailyTrigger
+    RunLevel    = 'Limited'
+    Description = 'Splatting is easy to read'
+}
+# I don't actually want to do this though :p
+# Register-ScheduledTask @registerTask
